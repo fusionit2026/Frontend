650179ee14b46c402b5536fad55478fa69e788ee
Amarasimhareddy Avula <amarasimha2001@gmail.com>
2026-05-28 06:26:09 +0530

    Fix CRA env vars for production URL

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_SOCKET_URL, {
+const socket = io(process.env.REACT_APP_SOCKET_URL, {
     transports: ["websocket"],
     withCredentials: true
 });
